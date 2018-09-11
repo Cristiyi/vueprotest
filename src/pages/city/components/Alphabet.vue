eda8a7941e68adbe5e8d4927016a405a502de4fa
Cristiyi <498935434@qq.com>
2018-09-11 22:37:14 +0800

    citylist

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+	<ul class="list">
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+	</ul>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'CityAlphabet'
+	}
+</script>
+
+<style type="text/css" lang="stylus" scoped> 
+	.list
+		display: flex
+		flex-direction: column
+		justify-content: center
+		position: absolute
+		top: 1.58rem
+		right: 0
+		bottom: 0
+		width: .4rem
+		.item
+			text-align: center
+			line-height: .4rem
+			color: #00bcd4
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,137 @@
+<template>
+	<div class="list" ref="wrapper">
+		<div>
+			<div class="area">
+				<div class="title border-topbottom">当前城市</div>
+				<div class="button-list">
+					<div class="button-wrap">
+						<div class="button">成都</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">热门城市</div>
+				<div class="button-list">
+					<div class="button-wrap">
+						<div class="button">成都</div>
+					</div>
+					<div class="button-wrap">
+						<div class="button">成都</div>
+					</div>
+					<div class="button-wrap">
+						<div class="button">成都</div>
+					</div>
+					<div class="button-wrap">
+						<div class="button">成都</div>
+					</div>
+					<div class="button-wrap">
+						<div class="button">成都</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+					<div class="item border-bottom">阿拉尔</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import Bscroll from 'better-scroll'
+	export default {
+		name: 'CityList',
+		 
+	}
+</script>
+
+<style type="text/css" lang="stylus" scoped> 
+	.border-topbottom
+		&:before
+			border-color: #ccc
+		&:after
+			border-color: #ccc
+	.border-bottom
+		&:before
+			border-color: #ccc
+	.list
+		overflow: hidden
+		position: absolute
+		top: 1.58rem
+		left: 0
+		right: 0
+		bottom: 0
+		.title
+			line-height: .54rem
+			background: #eee
+			padding-left: .2rem
+			color: #666
+			font-size: .26rem
+		.button-list
+			overflow: hidden
+			padding .1rem .6rem .1rem .1rem
+			.button-wrap
+				width: 33.33%
+				float: left
+				.button
+					text-align: center
+					margin: .1rem
+					padding: .1rem 0
+					border: .02rem solid #ccc
+					border-radius: .06rem
+		.item-list
+			.item
+				line-height: .76rem
+				padding-left: .2rem
+</style>
