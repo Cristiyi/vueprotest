e8ef74a711f6f538c4cd8d1696becc6cee9c4d13
Cristiyi <498935434@qq.com>
2018-09-18 23:12:39 +0800

    gallary

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+	<detail-banner></detail-banner>
+</template>	
+
+<script type="text/javascript">
+	import DetailBanner  from './components/Banner'
+	export default {
+		name: 'Home',
+		components: {
+			DetailBanner: DetailBanner,
+		},
+	}
+</script>
+
+<style type="text/css" lang="stylus" scoped>
+
+</style>
